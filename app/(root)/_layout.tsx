9b009f932bf4bd6531229ee9d3929aa397463bfe
Isacco-B <git@isaccobertoli.com>
2024-12-29 18:30:06 +0100

    add toast

--- app/(root)/_layout.tsx
+++ app/(root)/_layout.tsx
@@ -1,6 +1,22 @@
+import Toast, { BaseToast } from "react-native-toast-message";
 import { useAuth } from "@/context/AuthContext";
 import { MqttProvider } from "@/context/MqttContext";
 import { Redirect, Slot } from "expo-router";
+
+
+const toastConfig = {
+  success: (props: any) => (
+    <BaseToast
+      {...props}
+      style={{ borderLeftColor: "#00a3a4" }}
+      contentContainerStyle={{ paddingHorizontal: 15 }}
+      text1Style={{
+        fontSize: 15,
+        fontWeight: "400",
+      }}
+    />
+  ),
+};
 
 export default function AppLayout() {
   const { isAuthenticated } = useAuth();
@@ -10,6 +26,7 @@ export default function AppLayout() {
   return (
     <MqttProvider>
       <Slot />
+      <Toast config={toastConfig}/>
     </MqttProvider>
   );
 }
